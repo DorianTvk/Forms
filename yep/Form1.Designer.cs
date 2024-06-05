c585a16e99e4387803c15e2f17f34f0c98851b2d
Opilane TTHK <opilane@tthk.ee>
2024-06-05 15:15:50 +0300

    Forms commit

---
+++ yep/Form1.Designer.cs
@@ -0,0 +1,147 @@
+﻿namespace yep
+{
+    partial class MainForm
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            components = new System.ComponentModel.Container();
+            dataGridViewCategories = new DataGridView();
+            categoryIdDataGridViewTextBoxColumn = new DataGridViewTextBoxColumn();
+            nameDataGridViewTextBoxColumn = new DataGridViewTextBoxColumn();
+            categoryBindingSource = new BindingSource(components);
+            dataGridViewProducts = new DataGridView();
+            productIdDataGridViewTextBoxColumn = new DataGridViewTextBoxColumn();
+            nameDataGridViewTextBoxColumn1 = new DataGridViewTextBoxColumn();
+            productsBindingSource = new BindingSource(components);
+            Button1 = new Button();
+            ((System.ComponentModel.ISupportInitialize)dataGridViewCategories).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)categoryBindingSource).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)dataGridViewProducts).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)productsBindingSource).BeginInit();
+            SuspendLayout();
+            // 
+            // dataGridViewCategories
+            // 
+            dataGridViewCategories.AutoGenerateColumns = false;
+            dataGridViewCategories.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridViewCategories.Columns.AddRange(new DataGridViewColumn[] { categoryIdDataGridViewTextBoxColumn, nameDataGridViewTextBoxColumn });
+            dataGridViewCategories.DataSource = categoryBindingSource;
+            dataGridViewCategories.Location = new Point(12, 12);
+            dataGridViewCategories.Name = "dataGridViewCategories";
+            dataGridViewCategories.RowTemplate.Height = 25;
+            dataGridViewCategories.Size = new Size(240, 150);
+            dataGridViewCategories.TabIndex = 0;
+            dataGridViewCategories.CellContentClick += dataGridViewCategories_CellContentClick;
+            dataGridViewCategories.SelectionChanged += dataGridViewCategories_SelectionChanged;
+            // 
+            // categoryIdDataGridViewTextBoxColumn
+            // 
+            categoryIdDataGridViewTextBoxColumn.DataPropertyName = "CategoryId";
+            categoryIdDataGridViewTextBoxColumn.HeaderText = "CategoryId";
+            categoryIdDataGridViewTextBoxColumn.Name = "categoryIdDataGridViewTextBoxColumn";
+            categoryIdDataGridViewTextBoxColumn.ReadOnly = true;
+            // 
+            // nameDataGridViewTextBoxColumn
+            // 
+            nameDataGridViewTextBoxColumn.DataPropertyName = "Name";
+            nameDataGridViewTextBoxColumn.HeaderText = "Name";
+            nameDataGridViewTextBoxColumn.Name = "nameDataGridViewTextBoxColumn";
+            // 
+            // categoryBindingSource
+            // 
+            categoryBindingSource.DataSource = typeof(GetStartedWinForms.Category);
+            // 
+            // dataGridViewProducts
+            // 
+            dataGridViewProducts.AutoGenerateColumns = false;
+            dataGridViewProducts.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridViewProducts.Columns.AddRange(new DataGridViewColumn[] { productIdDataGridViewTextBoxColumn, nameDataGridViewTextBoxColumn1 });
+            dataGridViewProducts.DataSource = productsBindingSource;
+            dataGridViewProducts.Location = new Point(258, 12);
+            dataGridViewProducts.Name = "dataGridViewProducts";
+            dataGridViewProducts.RowTemplate.Height = 25;
+            dataGridViewProducts.Size = new Size(240, 150);
+            dataGridViewProducts.TabIndex = 1;
+            // 
+            // productIdDataGridViewTextBoxColumn
+            // 
+            productIdDataGridViewTextBoxColumn.DataPropertyName = "ProductId";
+            productIdDataGridViewTextBoxColumn.HeaderText = "ProductId";
+            productIdDataGridViewTextBoxColumn.Name = "productIdDataGridViewTextBoxColumn";
+            productIdDataGridViewTextBoxColumn.ReadOnly = true;
+            // 
+            // nameDataGridViewTextBoxColumn1
+            // 
+            nameDataGridViewTextBoxColumn1.DataPropertyName = "Name";
+            nameDataGridViewTextBoxColumn1.HeaderText = "Name";
+            nameDataGridViewTextBoxColumn1.Name = "nameDataGridViewTextBoxColumn1";
+            // 
+            // productsBindingSource
+            // 
+            productsBindingSource.DataMember = "Products";
+            productsBindingSource.DataSource = categoryBindingSource;
+            // 
+            // Button1
+            // 
+            Button1.Location = new Point(423, 168);
+            Button1.Name = "Button1";
+            Button1.Size = new Size(75, 23);
+            Button1.TabIndex = 2;
+            Button1.Text = "Save";
+            Button1.UseVisualStyleBackColor = true;
+            Button1.Click += Button1_Click;
+            // 
+            // MainForm
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(509, 211);
+            Controls.Add(Button1);
+            Controls.Add(dataGridViewProducts);
+            Controls.Add(dataGridViewCategories);
+            Name = "MainForm";
+            Text = "Products and Categories";
+            ((System.ComponentModel.ISupportInitialize)dataGridViewCategories).EndInit();
+            ((System.ComponentModel.ISupportInitialize)categoryBindingSource).EndInit();
+            ((System.ComponentModel.ISupportInitialize)dataGridViewProducts).EndInit();
+            ((System.ComponentModel.ISupportInitialize)productsBindingSource).EndInit();
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private DataGridView dataGridViewCategories;
+        private DataGridView dataGridViewProducts;
+        private Button Button1;
+        private BindingSource categoryBindingSource;
+        private BindingSource productsBindingSource;
+        private DataGridViewTextBoxColumn categoryIdDataGridViewTextBoxColumn;
+        private DataGridViewTextBoxColumn nameDataGridViewTextBoxColumn;
+        private DataGridViewTextBoxColumn productIdDataGridViewTextBoxColumn;
+        private DataGridViewTextBoxColumn nameDataGridViewTextBoxColumn1;
+    }
+}
---
+++ yep/ProductsContext.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace GetStartedWinForms;
+
+public class ProductsContext : DbContext
+{
+    public DbSet<Product> Products { get; set; }
+    public DbSet<Category> Categories { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        => optionsBuilder.UseSqlite("Data Source=products.db");
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Category>().HasData(
+            new Category { CategoryId = 1, Name = "Mercedes" },
+            new Category { CategoryId = 2, Name = "BMW" },
+            new Category { CategoryId = 3, Name = "KIA" },
+            new Category { CategoryId = 4, Name = "Fiat" });
+
+        modelBuilder.Entity<Product>().HasData(
+            new Product { ProductId = 1, CategoryId = 1, Name = "Must" },
+            new Product { ProductId = 2, CategoryId = 1, Name = "Valge" },
+            new Product { ProductId = 3, CategoryId = 1, Name = "Hall" },
+            new Product { ProductId = 4, CategoryId = 1, Name = "Hõbe" },
+            new Product { ProductId = 5, CategoryId = 1, Name = "Pruun" },
+            new Product { ProductId = 6, CategoryId = 1, Name = "Punane" },
+            new Product { ProductId = 7, CategoryId = 1, Name = "Kollane" },
+            new Product { ProductId = 8, CategoryId = 1, Name = "Sinine" },
+            new Product { ProductId = 9, CategoryId = 1, Name = "Lilla" },
+            new Product { ProductId = 10, CategoryId = 2, Name = "Must" },
+            new Product { ProductId = 11, CategoryId = 2, Name = "Valge" },
+            new Product { ProductId = 12, CategoryId = 2, Name = "Hall" },
+            new Product { ProductId = 13, CategoryId = 2, Name = "Hõbe" },
+            new Product { ProductId = 14, CategoryId = 2, Name = "Pruun" },
+            new Product { ProductId = 15, CategoryId = 2, Name = "Punane" },
+            new Product { ProductId = 16, CategoryId = 3, Name = "Must" },
+            new Product { ProductId = 17, CategoryId = 3, Name = "Valge" },
+            new Product { ProductId = 18, CategoryId = 3, Name = "Hall" },
+            new Product { ProductId = 19, CategoryId = 3, Name = "Kollane" },
+            new Product { ProductId = 20, CategoryId = 3, Name = "Pruun" },
+            new Product { ProductId = 21, CategoryId = 4, Name = "Must" },
+            new Product { ProductId = 22, CategoryId = 4, Name = "Valge" },
+            new Product { ProductId = 23, CategoryId = 4, Name = "Hõbe" },
+            new Product { ProductId = 24, CategoryId = 4, Name = "Kollane" },
+            new Product { ProductId = 25, CategoryId = 4, Name = "Sinine" },
+            new Product { ProductId = 26, CategoryId = 4, Name = "Lilla" });
+    }
+}
